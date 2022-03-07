cca3bbe90ec2b65bba4c9278db25a5107c19014a
Neha Sontakke <wasnik.neha@gmail.com>
2022-03-07 23:33:30 +0530

    added the coupon code

---
+++ About.js
@@ -0,0 +1,9 @@
+function loadCoupon(){
+    document.getElementById('coupon').style.visibility = 'visible';
+    document.body.style.backgroundColor = "black";
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility = 'hidden';
+    document.body.style.backgroundColor = "white";
+}
