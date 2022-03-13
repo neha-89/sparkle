e35eec91ef0daea43700b238ad66deca24ea8fa8
Neha Sontakke <wasnik.neha@gmail.com>
2022-03-13 16:34:20 +0530

    added the coupon

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function darkmode() {
     document.body.classList.toggle('dark-mode');
   }
+
+
+  function loadCoupon(){
+    document.getElementById('coupon').style.visibility = 'visible';
+    
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility = 'hidden';
+    
+}
