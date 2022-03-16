963d10dc006de53cdd7538114d6c916b58f80d81
Neha Sontakke <wasnik.neha@gmail.com>
2022-03-16 23:38:50 +0530

    added the pages design

--- About.js
+++
@@ -1,9 +0,0 @@
-function loadCoupon(){
-    document.getElementById('coupon').style.visibility = 'visible';
-    document.body.style.backgroundColor = "black";
-}
-
-function closeCoupon(){
-    document.getElementById('coupon').style.visibility = 'hidden';
-    document.body.style.backgroundColor = "white";
-}
