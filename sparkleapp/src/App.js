3d78e0f587952663d30acae7abbd366ed4134344
Neha Sontakke <wasnik.neha@gmail.com>
2022-05-23 22:33:06 +0530

    initial setup of sparkle usng react

---
+++ sparkleapp/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {BrowserRouter,Routes,Route,Link} from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header  />
+      <Footer />
+     
+    </div>
+  );
+}
+
+export default App;
