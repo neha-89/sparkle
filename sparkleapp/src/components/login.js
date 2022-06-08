2caf809d920d3002c6f201a1803147160dcc2174
Neha Sontakke <wasnik.neha@gmail.com>
2022-06-08 22:39:50 +0530

    updated

--- sparkleapp/src/components/login.js
+++ sparkleapp/src/components/login.js
@@ -1,76 +1,115 @@
-import React,{Component} from 'react';
-import Header from '../../header.js'
+import React, { useState } from 'react';
 
-const url = "http://localhost:5000/api/auth/login";
-
-class Login extends Component {
-    constructor(props){
-        super(props)
-
-        this.state={
-            email:'saloni@gmail.com',
-            password:'12345678',
-            message:''
-        }
+const url = "https://sparkleloginapi.herokuapp.com/api/auth/login";
+export default function Login(){
+    const [user,setUser] = useState('')
+    const [userDetails ,setUserDetails] = useState({
+        email :'',
+        password :''
+    }) 
+    const handleChange =(event)=>{
+            setUser(event.target.value);
     }
-
-    handleChange=(event) => {
-        this.setState({[event.target.name]:event.target.value})
-    }
-
-    login = () => {
-        fetch(url,{
-            method:'POST',
+    const login = () =>{
+        fetch(url,{method:POST
+            ,
             headers:{
                 'accept':'application/json',
                 'content-type':'application/json'
             },
-            body:JSON.stringify(this.state)
-        })
-        .then((res) => res.json())
-        .then((data) => {
-            if(data.auth === false){
-                this.setState({message:data.token})
-            }else{
-                sessionStorage.setItem('ltk',data.token)
-                this.props.history.push('/')
-            }
-        })
+            body:JSON.stringify(userDetails)})
     }
-
-    render(){
-        return(
-            <>
-            <Header/>
-            <div className="container">
-                <hr/>
-                <div className="panel panel-success">
-                    <div className="panel-heading">
-                        Login
-                    </div>
-                    <div className="panel-body">
-                        <h3 style={{color:'red'}}>{this.state.message}</h3>
-                        <div className="row">
-                            <div className="form-group col-md-6">
-                                <label for="email">Email</label>
-                                <input id="email" name="email" className="form-control"
-                                value={this.state.email} onChange={this.handleChange}/>
-                            </div>
-                             <div className="form-group col-md-6">
-                                <label for="password">Password</label>
-                                <input id="password" name="password" className="form-control"
-                                value={this.state.password} onChange={this.handleChange}/>
-                            </div>
-                            
-                        </div>
-                        <button className="btn btn-info" onClick={this.login}>Login</button>
-                    </div>
-                </div>
-            </div>
-            </>
-        )
-    }
-
+    return(
+        <>
+        <form >
+        <div className="mb-3 mt-3">
+            <label for="email" className="form-label">Email:</label>
+            <input type="email" className="form-control" id="email" value={userDetails.email} placeholder="Enter email" name="email" onChange= {handleChange}/>
+        </div>
+        <div class="mb-3">
+            <label for="pwd" class="form-label">Password:</label>
+            <input type="password" class="form-control" id="pwd" value = {userDetails.password} placeholder="Enter password" name="pswd" onChange={handleChange}/>
+        </div>
+        
+        <button type="submit" onClick = {login} class="btn btn-primary">Submit</button>
+        </form>
+        </>
+    )
+    
 }
+// import React,{Component} from 'react';
+// import Header from '../../header.js'
+
+// const url = "http://localhost:5000/api/auth/login";
+
+// class Login extends Component {
+//     constructor(props){
+//         super(props)
+
+//         this.state={
+//             email:'saloni@gmail.com',
+//             password:'12345678',
+//             message:''
+//         }
+//     }
+
+//     handleChange=(event) => {
+//         this.setState({[event.target.name]:event.target.value})
+//     }
+
+//     login = () => {
+//         fetch(url,{
+//             method:'POST',
+//             headers:{
+//                 'accept':'application/json',
+//                 'content-type':'application/json'
+//             },
+//             body:JSON.stringify(this.state)
+//         })
+//         .then((res) => res.json())
+//         .then((data) => {
+//             if(data.auth === false){
+//                 this.setState({message:data.token})
+//             }else{
+//                 sessionStorage.setItem('ltk',data.token)
+//                 this.props.history.push('/')
+//             }
+//         })
+//     }
+
+//     render(){
+//         return(
+//             <>
+//             <Header/>
+//             <div className="container">
+//                 <hr/>
+//                 <div className="panel panel-success">
+//                     <div className="panel-heading">
+//                         Login
+//                     </div>
+//                     <div className="panel-body">
+//                         <h3 style={{color:'red'}}>{this.state.message}</h3>
+//                         <div className="row">
+//                             <div className="form-group col-md-6">
+//                                 <label for="email">Email</label>
+//                                 <input id="email" name="email" className="form-control"
+//                                 value={this.state.email} onChange={this.handleChange}/>
+//                             </div>
+//                              <div className="form-group col-md-6">
+//                                 <label for="password">Password</label>
+//                                 <input id="password" name="password" className="form-control"
+//                                 value={this.state.password} onChange={this.handleChange}/>
+//                             </div>
+                            
+//                         </div>
+//                         <button className="btn btn-info" onClick={this.login}>Login</button>
+//                     </div>
+//                 </div>
+//             </div>
+//             </>
+//         )
+//     }
+
+// }
 
 export default Login
