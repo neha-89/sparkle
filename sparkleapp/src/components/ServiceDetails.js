3c1c0465a6cd4c8385eeae22a9042f6222c4af69
Neha Sontakke <wasnik.neha@gmail.com>
2022-07-27 22:14:58 +0530

    added

--- sparkleapp/src/components/ServiceDetails.js
+++ sparkleapp/src/components/ServiceDetails.js
@@ -1,23 +1,23 @@
-// import React,{useState ,useEffect} from 'react';
-// import axios from 'axios';
-// import { useParams } from 'react-router';
+import React,{useState ,useEffect} from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router';
 
 
-// export default function ServiceDetails(){
-//     const params = useParams();
-//     const [data ,setData] = useState([]);
+export default function ServiceDetails(){
+    const params = useParams();
+    const [data ,setData] = useState([]);
 
 
-//     useEffect(()=>{
-//         axios.get(`https://sparklenodeapi.herokuapp.com/api/subservice/service_id/${params.service_id}}`)
-//         .then((res)=>{res.json})
-//         .then((data) =>console.log(data))
-//         .then((data)=> setData(data))
-//     },[params.service_id])
+    useEffect(()=>{
+        axios.get(`https://sparklenodeapi.herokuapp.com/api/subservice/service_id/${params.service_id}}`)
+        .then((res)=>{res.json})
+        .then((data) =>console.log(data))
+        .then((data)=> setData(data))
+    },[params.service_id])
 
-//     return(
-//         <>
-//           <h1></h1>
-//         </>
-//     )
-// }
+    return(
+        <>
+          <h1></h1>
+        </>
+    )
+}
